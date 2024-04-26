e61113469c88a77fa4661864e8586d46130f9481
sbaecker2005 <144857264+sbaecker2005@users.noreply.github.com>
2024-04-26 13:47:47 -0300

    Update contatos.c

--- contatos.c
+++ contatos.c
@@ -1,1 +1,18 @@
+void listarContatos() {
+  printf("Listando contatos da sua agenda:\n");
 
+  FILE *arquivo = fopen("agenda.bin", "r");
+  if (arquivo == NULL) {
+    printf("Erro ao abrir o arquivo para listar os contatos!\n");
+    return;
+  }
+
+  Contato contato;
+  while (fscanf(arquivo, "%49s %49s %49s %11s", contato.nome, contato.sobrenome,
+                contato.email, contato.telefone) == 4) {
+    printf("%s %s, %s, %s\n", contato.nome, contato.sobrenome, contato.email,
+           contato.telefone);
+  }
+
+  fclose(arquivo);
+}
