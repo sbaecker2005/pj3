50f46ddf5a215cb7c0fd37940367fc0442806ab4
sbaecker2005 <144857264+sbaecker2005@users.noreply.github.com>
2024-04-26 13:37:52 -0300

    Update main.c

--- main.c
+++ main.c
@@ -8,3 +8,45 @@ void deletarContatos(Agenda *agenda);
 int main() {
   Agenda agenda;
   inicializarAgenda(&agenda);
+
+
+  int opcao;
+  do {
+    printf("\nMenu:\n");
+    printf("1. Adicionar Contato\n");
+    printf("2. Listar Contatos\n");
+    printf("3. Deletar Contato\n");
+    printf("4. Salvar Agenda\n");
+    printf("5. Carregar Agenda\n");
+    printf("6. Sair\n");
+    printf("Escolha uma opcao: ");
+    scanf("%d", &opcao);
+    limparBuffer();
+
+    switch (opcao) {
+    case 1:
+      adicionarContato(&agenda);
+      break;
+    case 2:
+      listarContatos(&agenda);
+      break;
+    case 3:
+      deletarContatos(&agenda);
+      break;
+    case 4:
+      salvarAgenda(&agenda);
+      break;
+    case 5:
+      carregarAgenda(&agenda);
+      break;
+    case 6:
+      printf("Saindo...\n");
+      break;
+    default:
+      printf("Opcao invalida!\n");
+    }
+  } while (opcao != 6);
+
+  return 0;
+}
+
