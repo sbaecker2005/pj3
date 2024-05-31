309bec391b762c1e06903c4064e0fbd6b93dc6a5
sbaecker2005 <144857264+sbaecker2005@users.noreply.github.com>
2024-05-31 19:01:53 -0300

    Update main.c

--- main.c
+++ main.c
@@ -5,47 +5,53 @@
 
 void deletarContatos(Agenda *agenda);
 
+void menu(Agenda *agenda) {
+    int opcao;
+    do {
+        printf("\nMenu:\n");
+        printf("1. Adicionar Contato\n");
+        printf("2. Listar Contatos\n");
+        printf("3. Deletar Contato\n");
+        printf("4. Salvar Agenda\n");
+        printf("5. Carregar Agenda\n");
+        printf("6. Sair\n");
+        printf("Escolha uma opcao: ");
+        if (scanf("%d", &opcao) != 1) {
+            printf("Erro ao ler a opcao!\n");
+            limparBuffer();
+            continue;
+        }
+        limparBuffer();
+
+        switch (opcao) {
+            case 1:
+                adicionarContato(agenda);
+                break;
+            case 2:
+                listarContatos(agenda);
+                break;
+            case 3:
+                deletarContatos(agenda);
+                break;
+            case 4:
+                salvarAgenda(agenda);
+                break;
+            case 5:
+                carregarAgenda(agenda);
+                break;
+            case 6:
+                printf("Saindo...\n");
+                break;
+            default:
+                printf("Opcao invalida!\n");
+        }
+    } while (opcao != 6);
+}
+
 int main() {
-  Agenda agenda;
-  inicializarAgenda(&agenda);
-  carregarAgenda(&agenda);
-
-  int opcao;
-  do {
-    printf("\nMenu:\n");
-    printf("1. Adicionar Contato\n");
-    printf("2. Listar Contatos\n");
-    printf("3. Deletar Contato\n");
-    printf("4. Salvar Agenda\n");
-    printf("5. Carregar Agenda\n");
-    printf("6. Sair\n");
-    printf("Escolha uma opcao: ");
-    scanf("%d", &opcao);
-    limparBuffer();
-
-    switch (opcao) {
-    case 1:
-      adicionarContato(&agenda);
-      break;
-    case 2:
-      listarContatos(&agenda);
-      break;
-    case 3:
-      deletarContatos(&agenda);
-      break;
-    case 4:
-      salvarAgenda(&agenda);
-      break;
-    case 5:
-      carregarAgenda(&agenda);
-      break;
-    case 6:
-      printf("Saindo...\n");
-      break;
-    default:
-      printf("Opcao invalida!\n");
-    }
-  } while (opcao != 6);
-
-  return 0;
+    Agenda agenda;
+    inicializarAgenda(&agenda);
+    carregarAgenda(&agenda);
+    menu(&agenda);
+    return 0;
 }
